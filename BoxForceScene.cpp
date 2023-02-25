e1fd606c3fd76c7d94addd46375d9fc72ac885ae
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2023-02-25 13:55:33 +0100

    Created Box Force Scene

---
+++ BoxForceScene.cpp
@@ -0,0 +1,22 @@
+#include "stdafx.h"
+#include "BoxForceScene.h"
+
+void BoxForceScene::Initialize()
+{
+}
+
+void BoxForceScene::Update()
+{
+}
+
+void BoxForceScene::Draw() const
+{
+}
+
+void BoxForceScene::OnSceneActivated()
+{
+}
+
+void BoxForceScene::OnSceneDeactivated()
+{
+}
---
+++ BoxForceScene.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "GameScene.h"
+class BoxForceScene final : public GameScene
+{
+public:
+	BoxForceScene() :GameScene(L"BoxForceScene") {}
+	~BoxForceScene() override = default;
+	BoxForceScene(const BoxForceScene& other) = delete;
+	BoxForceScene(BoxForceScene&& other) noexcept = delete;
+	BoxForceScene& operator=(const BoxForceScene& other) = delete;
+	BoxForceScene& operator=(BoxForceScene&& other) noexcept = delete;
+
+protected:
+	void Initialize() override;
+	void Update() override;
+	void Draw() const override;
+	void OnSceneActivated() override;
+	void OnSceneDeactivated() override;
+
+private:
+
+};
